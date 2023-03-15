bd678c16123dcdbbe2e182e64c0939b2d666a443
Eric Thvedt <ericsthvedt@gmail.com>
2023-03-14 18:15:30 -0700

    passed all tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_seq = []
+    if length > 0:
+        for i in range(length):
+            if i == 0:
+                fib_seq.append(0)
+            elif i == 1:
+                fib_seq.append(1)
+            else:
+                new_fib = fib_seq[-2] + fib_seq[-1]
+                fib_seq.append(new_fib)
+    print(fib_seq)
